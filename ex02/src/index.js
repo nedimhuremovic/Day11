92eefb0b50e4ebaaf74026fe6343cdebd2613ef7
nedimhuremovic <nedimhuremovic.@gmail.com>
2021-03-18 11:48:04 +0100

    ex02 added

---
+++ ex02/src/index.js
@@ -0,0 +1,26 @@
+// Create a temps array here
+    var avgTemp = [
+        [31,32,19,37],
+        [29,27,55,36],
+        [17,27,42,46],
+        [29,52,21,64],
+        [91,27,31,61]
+    ];
+// End of temps array
+function myArrayFunction(arr) {
+    var newTemps = [...arr];
+    var averageDayTemp = [];
+  // Only change code below this line
+    var sum = 0;
+        for (var i = 0; i < 5; i++) {
+            sum = 0;
+                for (var j = 0; j < 4; j++) {
+                    sum += newTemps[i][j];
+            }
+          averageDayTemp.push(sum/4);
+        }
+  // Only change code above this line
+  return averageDayTemp;
+}
+console.log(myArrayFunction(avgTemp)); // Change this line
+module.exports = myArrayFunction;
