00935b7d4725db533100f2492811712ee195ca7a
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-18 22:25:31 +0100

    ex10 added tempArray because input arrays should remain the same

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,10 +1,12 @@
 // Only change code below this line
     function mySplice(arr1, arr2, n) {
-       for (let i = arr1.length-1; i >= 0; i--) {
-        arr2.splice(n,0,arr1[i]);
+      var tempArray = [...arr2];
+      
+      for (let i = arr1.length-1; i >= 0; i--) {
+         tempArray.splice(n,0,arr1[i]);
         n++;                      
        }
-       return arr2;
+       return tempArray;
     }
   // Only change code above this line
   console.log(mySplice([1,2,3],[4,5],2)); // Change this line
