9800c1ba4aeb109624c0e2b051109130c350590b
nedimhuremovic <nedimhuremovic.@gmail.com>
2021-03-18 15:11:37 +0100

    ex05 added

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function myNestedFunction(arr) {
+    arr.push(["Milka", 3]);
+    return arr;
+}
+var myNestedArray = [["Toblerone", 5]];
+
+// Only change code above this line
+console.log(myNestedFunction(myNestedArray)); // Change this line
+module.exports = myNestedFunction;
